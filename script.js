322744a079e6543e0144ab054ba203b328eb1680
seppe_verhavert <seppe.verhavert@gmail.com>
2019-09-02 11:08:53 +0200

    2.1

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
 /* becode/javascript
  *
- * /01-base/01-var-alert/script.js - 1.1: var & alert
+ * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
  *
  * coded by leny@BeCode
  * started at 26/10/2018
@@ -10,26 +9,29 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-function askInfo() {
-    let userAge = prompt("What is your age?");
-    if (userAge != null) {
-        let userGender = prompt("What is your gender?");
-        if (userGender != null) {
-            let userTown = prompt("Where do you live?");
-            if (userGender != null) {
-                var userInfo = confirm(" is this info correct? " + userAge + " " + userGender + " " + userTown);
-                if(!userInfo) {
-                    askInfo();
-                }
-            } else {
-                askInfo();
-            }
-        } else {
-            askInfo();
-        }
-    } else {
-        askInfo();
-    }
-};
+(function () {
+    let firstOperator = parseInt(document.getElementById("op-one").value);
+    let secondOperator = parseInt(document.getElementById("op-two").value);
+    let output = document.getElementsByClassName('explain')[0];
+    let solution;
 
-askInfo();
+    document.getElementById("addition").addEventListener("click", function () {
+        solution = firstOperator + secondOperator;
+        output.innerHTML = solution;
+    });
+
+    document.getElementById("substraction").addEventListener("click", function () {
+        solution = firstOperator - secondOperator;
+        output.innerHTML = solution;
+    });
+
+    document.getElementById("multiplication").addEventListener("click", function () {
+        solution = firstOperator * secondOperator;
+        output.innerHTML = solution;
+    });
+
+    document.getElementById("division").addEventListener("click", function () {
+        solution = firstOperator / secondOperator;
+        output.innerHTML = solution;
+    });
+})();
