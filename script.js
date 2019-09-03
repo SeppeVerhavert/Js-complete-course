519c5adc9936e616caf79a76ce72a692d0b6751a
seppe_verhavert <seppe.verhavert@gmail.com>
2019-09-03 11:24:57 +0200

    2.5

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 /* becode/javascript
  *
- * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
+ * /02-maths/05-factorial/script.js - 2.5: Factorielle
  *
  * coded by leny@BeCode
  * started at 26/10/2018
@@ -10,24 +11,18 @@
 // You will have time to focus on it later.
 
 (function () {
-    let numbersInput = document.getElementById("numbers").value;
-    document.getElementById("run").addEventListener('click', sortNumbers);
-    let array = [];
-    let sortedArray = [];
 
-    function sortNumbers() {
-        array = numbersInput.split(", ");
-        for (let i = 0; i < array.length; i++) {
-            let numberMin = 100;
-            for (let x = 0; x < array.length; x++) {
-                if (parseInt(array[x]) < numberMin) {
-                    numberMin = array[x];
-                }
+    document.getElementById("run").addEventListener("click", function () {
+        let numberInput = parseInt(document.getElementById("number").value);
+        let solution = 1;
+
+        if (numberInput == 0) {
+            alert(solution);
+        } else {
+            for (let i = numberInput; i > 0; i--) {
+                solution = solution * i;
             }
-            sortedArray[i] = parseInt(numberMin);
-            let minIndex = array.indexOf(numberMin);
-            array[minIndex] = "";
+            alert(solution);
         }
-        alert(sortedArray);
-    }
+    });
 })();
