ccf7521975796577a60fc84ffae55a6e3eaa11a5
seppe_verhavert <seppe.verhavert@gmail.com>
2019-09-03 09:40:30 +0200

    2.2

--- script.js
+++ script.js
@@ -10,28 +10,34 @@
 // You will have time to focus on it later.
 
 (function () {
+    let operators = document.getElementsByClassName('operator');
+    let output = document.getElementsByClassName('explain')[0];
     let firstOperator = parseInt(document.getElementById("op-one").value);
     let secondOperator = parseInt(document.getElementById("op-two").value);
-    let output = document.getElementsByClassName('explain')[0];
     let solution;
 
-    document.getElementById("addition").addEventListener("click", function () {
-        solution = firstOperator + secondOperator;
-        output.innerHTML = solution;
-    });
+    for (let x = 0; x < operators.length; x++) {
+        operators[x].addEventListener('click', checkId);
+    }
 
-    document.getElementById("substraction").addEventListener("click", function () {
-        solution = firstOperator - secondOperator;
-        output.innerHTML = solution;
-    });
-
-    document.getElementById("multiplication").addEventListener("click", function () {
-        solution = firstOperator * secondOperator;
-        output.innerHTML = solution;
-    });
-
-    document.getElementById("division").addEventListener("click", function () {
-        solution = firstOperator / secondOperator;
-        output.innerHTML = solution;
-    });
+    function checkId() {
+        switch (this.id) {
+            case "addition":
+                solution = firstOperator + secondOperator;
+                output.innerHTML = solution;
+                break;
+            case "substraction":
+                solution = firstOperator - secondOperator;
+                output.innerHTML = solution;
+                break;
+            case "multiplication":
+                solution = firstOperator * secondOperator;
+                output.innerHTML = solution;
+                break;
+            case "division":
+                solution = firstOperator / secondOperator;
+                output.innerHTML = solution;
+                break;
+        }
+    }
 })();
