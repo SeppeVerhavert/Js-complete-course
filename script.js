cbc1fe9567227ef370a78fb47fac1d2dc678e2ea
seppe_verhavert <seppe.verhavert@gmail.com>
2019-09-04 10:13:33 +0200

    5.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* becode/javascript
  *
- * /04-dates/01-title-by-hour-one/script.js - 4.1: texte en fonction de l'heure (1)
+ * /05-arrays/01-get-element/script.js - 5.1: élément de tableau
  *
  * coded by leny@BeCode
  * started at 26/10/2018
@@ -11,20 +11,23 @@
 
 (function () {
 
-    document.getElementById('run').addEventListener('click', calculateFriday);
+    var fruits = [
+        "apple",
+        "pear",
+        "raspberry",
+        "tomatos",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peache",
+        "grap",
+        "cherry",
+    ];
 
-    function calculateFriday() {
-        let yearInput = document.getElementById('year').value;
-        let spookyFridays = [];
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-        for (let i = 0; i < months.length; i++) {
-            let newDate = new Date( months[i] + '13, ' + yearInput +  ' 00:00:00');
-            let newDay = newDate.getDay();
-            if (newDay === 5) {
-                spookyFridays.push(newDate);
-            }
-        }
-        alert(spookyFridays);
+    document.getElementById('run').addEventListener('click', getElement);
+    function getElement() {
+        console.log(fruits[3]);
     }
 })();
