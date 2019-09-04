70ecd859bc5afca90922448bc5d133e1d116bf82
seppe_verhavert <seppe.verhavert@gmail.com>
2019-09-04 11:13:46 +0200

    5.5

--- script.js
+++ script.js
@@ -11,25 +11,38 @@
 
 (function () {
 
-    var fruits = [
-        "apple",
-        "pear",
-        "raspberry",
-        "tomatos",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peache",
-        "grap",
-        "cherry",
+    var people = [
+        {
+            firstname: "Garald",
+            lastname: "Freschi",
+            email: "gfreschi0@discuz.net",
+        },
+        {
+            firstname: "Bendicty",
+            lastname: "Foster-Smith",
+            email: "bfostersmith1@mysql.com",
+        },
+        {
+            firstname: "Michelle",
+            lastname: "Draude",
+            email: "mdraude2@csmonitor.com",
+        },
+        {
+            firstname: "Cathee",
+            lastname: "Meineck",
+            email: "cmeineck3@t-online.de",
+        },
+        {
+            firstname: "Marianna",
+            lastname: "Coal",
+            email: "mcoal4@creativecommons.org",
+        },
     ];
 
     document.getElementById('run').addEventListener('click', loopArray);
     function loopArray() {
-        fruits.forEach(element => {
-            console.log(element)
+        people.forEach(element => {
+            console.log(element.firstname + " " + element.lastname);
         });
     }
 })();
