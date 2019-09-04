5d804e98a834674f48ae5fa2365e4a507cc66cd7
seppe_verhavert <seppe.verhavert@gmail.com>
2019-09-04 10:10:15 +0200

    4.5

--- script.js
+++ script.js
@@ -11,10 +11,20 @@
 
 (function () {
 
-    let target = document.getElementById('target');
-    
-    setInterval(function(){
-        let date = new Date();
-        target.innerHTML = date;
-    }, 1000);
+    document.getElementById('run').addEventListener('click', calculateFriday);
+
+    function calculateFriday() {
+        let yearInput = document.getElementById('year').value;
+        let spookyFridays = [];
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        for (let i = 0; i < months.length; i++) {
+            let newDate = new Date( months[i] + '13, ' + yearInput +  ' 00:00:00');
+            let newDay = newDate.getDay();
+            if (newDay === 5) {
+                spookyFridays.push(newDate);
+            }
+        }
+        alert(spookyFridays);
+    }
 })();
