65f5bc2166b4974a1a3af2a19e633a1e44a0508b
seppe_verhavert <seppe.verhavert@gmail.com>
2019-09-05 10:13:28 +0200

    5.8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* becode/javascript
  *
- * /05-arrays/07-filter-array/script.js - 5.7: filtrer un tableau
+ * /05-arrays/08-array-includes/script.js - 5.8: présence dans un tableau
  *
  * coded by leny@BeCode
  * started at 26/10/2018
@@ -11,94 +11,28 @@
 
 (function() {
 
-    var people = [
-        {
-            firstname: "Dreddy",
-            lastname: "Nussgen",
-            age: 70,
-        },
-        {
-            firstname: "Yves",
-            lastname: "Sear",
-            age: 49,
-        },
-        {
-            firstname: "Marcel",
-            lastname: "Cowderay",
-            age: 59,
-        },
-        {
-            firstname: "Dag",
-            lastname: "Binnall",
-            age: 31,
-        },
-        {
-            firstname: "Horten",
-            lastname: "Claesens",
-            age: 75,
-        },
-        {
-            firstname: "Charmian",
-            lastname: "Harnes",
-            age: 10,
-        },
-        {
-            firstname: "Sean",
-            lastname: "Littlejohns",
-            age: 37,
-        },
-        {
-            firstname: "Hazlett",
-            lastname: "Sprouls",
-            age: 87,
-        },
-        {
-            firstname: "Marcel",
-            lastname: "Hasted",
-            age: 66,
-        },
-        {
-            firstname: "Cary",
-            lastname: "Summerson",
-            age: 15,
-        },
-        {
-            firstname: "Feodor",
-            lastname: "Ollett",
-            age: 56,
-        },
-        {
-            firstname: "Kelly",
-            lastname: "Ranahan",
-            age: 62,
-        },
-        {
-            firstname: "Madelene",
-            lastname: "Davie",
-            age: 14,
-        },
-        {
-            firstname: "Bent",
-            lastname: "Karpe",
-            age: 82,
-        },
-        {
-            firstname: "Reinaldo",
-            lastname: "Grimbleby",
-            age: 81,
-        },
+    var fruits = [
+        "apple",
+        "perry",
+        "strawberry",
+        "tomato",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peach",
+        "grapes",
+        "cherry",
     ];
 
-    document.getElementById('run').addEventListener('click', createArray);
-    let newArray = [];
-    function createArray() {
-        people.find(age);
-        console.log(newArray);
-    }
+    document.getElementById('run').addEventListener('click', checkApple);
 
-    function age(element){
-        if (element.age >= 18){
-            newArray.push(element);
+    function checkApple(){
+        if(fruits.indexOf("apple")>=0){
+            console.log("yes");
+        } else {
+            console.log("no");
         }
     }
 
